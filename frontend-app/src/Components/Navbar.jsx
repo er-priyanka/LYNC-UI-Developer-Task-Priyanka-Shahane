5efb8e24f74f7c16fe6bc0e65f7ec021c75cb8f2
er-priyanka <shahanepriyankas01@gmail.com>
2024-07-23 21:06:26 +0530

    added menubar and responsiveness

--- frontend-app/src/Components/Navbar.jsx
+++ frontend-app/src/Components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import infinityImage from "../Assets/Group_12.png"
 import { BsArrowRight } from "react-icons/bs";
+import { MdMenu } from "react-icons/md";
 
 // style={{backgroundImage:"linear-gradient(to right, #020617,rgba(63,55,201,0.5),#020617,rgba(63,55,201,0.45),#020617)"}}
 
 
 const Navbar = () => {
+    const [isMenubarVisible, setIsMenubarVisible] = useState(false);
+
+    const handleMenubar = ()=>{
+        setIsMenubarVisible(!isMenubarVisible);
+    }
+
   return (
     <div className='
     w-[100%]
     py-[62px]
-   
     '
     >
         {/* background div1 */}
@@ -48,15 +54,18 @@ const Navbar = () => {
         rounded-[48px]
         border
         '>
+            <div onClick={handleMenubar} className='block cursor-pointer lg:hidden'>
+                <MdMenu  className='text-[35px]' />
+            </div>
             <div>
                 <img src={infinityImage} alt='infinity image' />
             </div>
+
             <div className="
             w-[50%]
-            flex
+            lg:flex hidden
             justify-between
             items-center
-            
             ">
                 <a href="">Home</a>
                 <a href="">Documentation</a>
@@ -81,6 +90,26 @@ const Navbar = () => {
                 <BsArrowRight />
 
             </div>
+        </div>
+
+        {/* menu items */}
+
+        <div className="
+            w-[25%] py-[20px] rounded-[25px] 
+             flex-col gap-[20px]
+            justify-between
+            items-center bg-[#010412] border border-gray-600 
+            absolute  top-[130px] left-[50px] 
+            "
+            style={{
+                display: `${isMenubarVisible? 'flex':'none'}`
+            }}
+            >
+                <a href="">Home</a>
+                <a href="">Documentation</a>
+                <a href="">Blogs</a>
+                <a href="">LYNC Portal</a>
+                <a href="">Request a demo</a>
         </div>
     </div>
     
